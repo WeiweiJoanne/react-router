a90417f8329736787360da1b0f576eee23a796e5
joannewei <joannewei0921@gmail.com>
2022-08-30 22:26:32 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { Routes, Route, Link, Outlet } from 'react-router-dom'
+
+function Home() {
+  return <h1>Home</h1>
+}
+
+function FAQ() {
+  return <h1>FAQ</h1>
+}
+function Layout() {
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/FAQ">FAQ</Link>
+          </li>
+          <li>
+            <Link to="/tour">Tour</Link>
+          </li>
+        </ul>
+      </nav>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index path="/" element={<Home />}></Route>
+          <Route path="/FAQ" element={<FAQ />}></Route>
+          <Route path="/tour" element={<Tour />}>
+            <Route index element={<TourList />} />
+            <Route path="Id" element={<TourDisc />} />
+          </Route>
+
+
+        </Route>
+      </Routes>
     </div>
   );
 }
